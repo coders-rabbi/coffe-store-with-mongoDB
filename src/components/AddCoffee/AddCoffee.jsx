9adab2256371b60af82d58f56937108f321057e9
coders rabbi <pcrabbi2020@gmail.com>
2023-05-11 18:28:35 +0600

    sweet alert working good.

--- src/components/AddCoffee/AddCoffee.jsx
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
     const hangleAddCoffe = event => {
@@ -25,14 +27,24 @@ const AddCoffee = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                if(data.insertedId){
-                    alert('Coffe added successfully')
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Successfully Added',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
                     form.reset();
                 }
             })
     }
     return (
         <div>
+            <div>
+                <Link to="/">
+                    <span className='text-2xl font-semibold'>Back To Home</span>
+                </Link>
+            </div>
             <div className='p-10 bg-[#F4F3F0]'>
                 <h1 className='text-5xl font-extrabold text-center mb-6'>Add Coffe</h1>
                 <form onSubmit={hangleAddCoffe}>
@@ -43,7 +55,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Coffe Name</span>
                             </label>
                             <label>
-                                <input type="text" name='name' placeholder="Coffe Name" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='name' placeholder="Coffe Name" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                         <div className='form-control w-1/2'>
@@ -51,7 +63,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Chef</span>
                             </label>
                             <label>
-                                <input type="text" name='chef' placeholder="Enter Coffe Chef" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='chef' placeholder="Enter Coffe Chef" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                     </div>
@@ -62,7 +74,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Supplier</span>
                             </label>
                             <label>
-                                <input type="text" name='supplier' placeholder="Enter Coffe Supplier" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='supplier' placeholder="Enter Coffe Supplier" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                         <div className='form-control w-1/2'>
@@ -70,7 +82,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Taste</span>
                             </label>
                             <label>
-                                <input type="text" name='taste' placeholder="Enter Coffe taste" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='taste' placeholder="Enter Coffe taste" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                     </div>
@@ -81,7 +93,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Category</span>
                             </label>
                             <label>
-                                <input type="text" name='category' placeholder="Enter Coffe Category" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='category' placeholder="Enter Coffe Category" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                         <div className='form-control w-1/2'>
@@ -89,7 +101,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Details</span>
                             </label>
                             <label>
-                                <input type="text" name='details' placeholder="Enter Coffe Details" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='details' placeholder="Enter Coffe Details" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                     </div>
@@ -100,7 +112,7 @@ const AddCoffee = () => {
                                 <span className='label-text'>Photo</span>
                             </label>
                             <label>
-                                <input type="text" name='photo' placeholder="Enter photo url" className="border border-black py-2 px-3 text-xl w-full input input-rounded" />
+                                <input type="text" name='photo' placeholder="Enter photo url" className="border border-black py-2 px-3 text-xl w-full input input-rounded" required/>
                             </label>
                         </div>
                     </div>
